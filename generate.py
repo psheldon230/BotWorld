e9021ec4b213fa372b2994c6846c057565b19c1d
psheldon230 <pasheldon23@gmail.com>
2023-01-16 18:33:17 -0600

    Updating-

--- generate.py
+++ generate.py
@@ -1,12 +1,19 @@
 import pyrosim.pyrosim as pyrosim
-currpos = .5
-def CreateWorld():
+
+def Create_World():
     pyrosim.Start_SDF("world.sdf")
-    pyrosim.Send_Cube(name="Box", pos=[0,0, 0.5] , size=[1, 1, 1])
+    pyrosim.Send_Cube(name="Box", pos=[-3,3,0.5] , size=[1,1,1])
     pyrosim.End()
-def CreateRobot():
+    pass
+
+def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
-    pyrosim.Send_Cube(name="Torso", pos=[0,0, 0.5] , size=[1, 1, 1])
+    pyrosim.Send_Cube(name="Torso", pos=[0,0,1] , size=[1,1,1])
+    pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [0.5,0,0.5])
+    pyrosim.Send_Cube(name="BackLeg", pos=[0.5,0,-0.5] , size=[1,1,1])
+    pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [-0.5,0,0.5])    
+    pyrosim.Send_Cube(name="FrontLeg", pos=[-0.5,0,-0.5] , size=[1,1,1])
     pyrosim.End()
 
-CreateWorld()
+Create_World()
+Create_Robot()
